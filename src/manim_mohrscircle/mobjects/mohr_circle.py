67e2ac49ece5db5aded13afb8c4cae17128e04e5
senft-research <JordanSenft@proton.me>
2026-05-31 14:54:26 +0100

    Added a center point dot mobject, and then cleaned up my naming convention for dots vs data (was becoming a headache)

--- src/manim_mohrscircle/mobjects/mohr_circle.py
+++ src/manim_mohrscircle/mobjects/mohr_circle.py
@@ -7,6 +7,7 @@ class Points:
     def __init__(self, p1, p2):
         self.point_1 = p1
         self.point_2 = p2
+        self.center_point = (p1 + p2) / 2
 
 
 def init_points(stress_x, stress_y, stress_shear):
@@ -19,71 +20,79 @@ class MohrCircle(VGroup):
     def __init__(self, stress_x, stress_y, stress_shear, axes, **kwargs):
         super().__init__(**kwargs)
         self.axes = axes
-        self._init_stresses(stress_x, stress_y, stress_shear)
+        self._init_raw_data(stress_x, stress_y, stress_shear)
         self._create_objects()
         self._set_mobject_z_indices()
         self._add_mobjects()
 
-    def _init_stresses(self, stress_x, stress_y, stress_shear):
+    def _init_raw_data(self, stress_x, stress_y, stress_shear):
         self.stress_x = stress_x
         self.stress_y = stress_y
         self.stress_shear = stress_shear
-
-    def _create_objects(self):
-        self._create_dots()
-        self.line = Line(self.point_1.get_center(), self.point_2.get_center(), color=BLUE)
-        self._create_labels()
-        self._create_circle()
-
-    def _create_dots(self):
-        self.point_1 = Dot(point=self.axes.c2p(self.stress_x, -self.stress_shear), color=CYAN)
-        self.point_2 = Dot(point=self.axes.c2p(self.stress_y, self.stress_shear), color=CYAN)
-
-    def _create_labels(self):
-        self.label_1 = MathTex(f"({self.stress_x}, {-self.stress_shear})").next_to(self.point_1, UP)
-        self.label_2 = MathTex(f"({self.stress_y}, {self.stress_shear})").next_to(self.point_2, DOWN)
-
-    def _create_circle(self):
-        points = init_points(
+        self.circle_points = init_points(
             self.stress_x,
             self.stress_y,
             self.stress_shear
         )
 
-        self._init_key_geometry(points)
-        
+
+    def _create_objects(self):
+        self._create_initial_points()
+        self.line = Line(self.point_1_dot.get_center(), self.point_2_dot.get_center(), color=BLUE)
+        self._create_labels()
+        self._create_circle()
+
+    def _create_initial_points(self):
+        self.point_1_dot = Dot(point=self.axes.c2p(self.circle_points.point_1), color=CYAN)
+        self.point_2_dot = Dot(point=self.axes.c2p(self.circle_points.point_2), color=CYAN)
+        self.center_point_dot = Dot(point=self.axes.c2p(self.circle_points.center_point), color=RED)
+
+    def _create_labels(self):
+        self.label_1 = MathTex(f"({self.stress_x}, {-self.stress_shear})").next_to(self.point_1_dot, UP)
+        self.label_2 = MathTex(f"({self.stress_y}, {self.stress_shear})").next_to(self.point_2_dot, DOWN)
+
+    def _create_circle(self):
+
+
+        self._init_key_geometry()
+
         self.circle = ParametricFunction(
             lambda t: self.axes.c2p(self._create_circle_points(t)
             ),
             t_range=[0, TAU],
             color=RED,
         )
-        self._rotate_circle(points)
+
+        self._rotate_circle()
 
     def _add_mobjects(self):
         self.add(self.circle)
-        self.add(self.point_1, self.point_2)
+        self.add(self.point_1_dot, self.point_2_dot)
         self.add(self.label_1, self.label_2)
         self.add(self.line)
+        self.add(self.center_point_dot)
 
     def _set_mobject_z_indices(self):
         self.circle.set_z_index(0)
         self.line.set_z_index(1)
-        self.point_1.set_z_index(2)
-        self.point_2.set_z_index(2)
+        self.point_1_dot.set_z_index(2)
+        self.point_2_dot.set_z_index(2)
+        self.center_point_dot.set_z_index(2)
+        self.label_1.set_z_index(3)
+        self.label_2.set_z_index(3)
 
-    def _init_key_geometry(self, points):
-        self.center_point = (points.point_1 + points.point_2) / 2
+    def _init_key_geometry(self):
+        self.center_point = self.circle_points.center_point
         self.circle_radius = np.linalg.norm(
-            points.point_1 - points.point_2
+            self.circle_points.point_1 - self.circle_points.point_2
         ) / 2
 
     def _create_circle_points(self,t):
         return (self.center_point[0] + self.circle_radius * np.cos(t),
-        self.center_point[1] + self.circle_radius * np.sin(t))
+                self.center_point[1] + self.circle_radius * np.sin(t))
 
-    def _rotate_circle(self, points):
-        angle = self._find_point_angle(points)
+    def _rotate_circle(self):
+        angle = self._find_point_angle(self.circle_points)
         self.circle.rotate(angle, about_point=self.circle.get_center())
 
     def _find_point_angle(self, points):
